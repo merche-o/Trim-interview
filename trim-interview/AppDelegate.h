98ceb2eab1fc98f13977213cbff0b80f5cd75715
Ainur <o.mercher24@gmail.Com>
2018-06-19 12:42:44 +0800

    v1

---
+++ trim-interview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  trim-interview
+//
+//  Created by Ainur on 17/06/2018.
+//  Copyright © 2018 merche_o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
